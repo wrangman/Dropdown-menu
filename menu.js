d31c6533dde5be2a5d192feddf4c9be27ef4f60a
wrangman <wrangman@gmail.com>
2024-10-28 19:47:02 +0100

    fix anim

--- menu.js
+++ menu.js
@@ -2,8 +2,10 @@
 
 const primaryNavigation = document.getElementById("primary-navigation");
 const btnHamburger = document.getElementById("hamburger-button");
+const iconHamburger = btnHamburger.querySelector(".icon-hamburger");
 
 btnHamburger.addEventListener("click", () => {
     primaryNavigation.classList.toggle("open");
+    iconHamburger.classList.toggle("morphToClose");
     btnHamburger.setAttribute("aria-expanded", btnHamburger.getAttribute("aria-expanded") === "false" ? "true" : "false");
 })
