e0d0de82114c28bb23fe6330fafa5f15cba01cbe
wrangman <wrangman@gmail.com>
2025-11-06 11:24:57 +0100

    f

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ const btnHamburger = document.getElementById("hamburger-button");
 const iconHamburger = btnHamburger.querySelector(".icon-hamburger");
 
 btnHamburger.addEventListener("click", () => {
-    primaryNavigation.classList.toggle("open");
-    iconHamburger.classList.toggle("morphToClose");
-    btnHamburger.setAttribute("aria-expanded", btnHamburger.getAttribute("aria-expanded") === "false" ? "true" : "false");
+   primaryNavigation.classList.toggle("open");
+   iconHamburger.classList.toggle("morphToClose");
+   btnHamburger.setAttribute("aria-expanded", btnHamburger.getAttribute("aria-expanded") === "false" ? "true" : "false");
 })
